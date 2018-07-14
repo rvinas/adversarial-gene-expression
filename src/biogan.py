60d8fb942ec9a18223872c16bb309519a8a4eb47
Ramon Viñas Torné <rvinast@gmail.com>
2018-07-14 12:49:31 +0200

    Clean code

--- src/biogan.py
+++ src/biogan.py
@@ -1,164 +1,85 @@
-from keras.layers import Input, Dense, Dropout, LeakyReLU, BatchNormalization
+from keras.layers import Input, Dense, Dropout, LeakyReLU
 from keras.models import Model
-from keras.optimizers import Adam, RMSprop
+from keras.optimizers import Adam
 import numpy as np
 from data_pipeline import load_data, save_synthetic, reg_network, split_train_test, clip_outliers
 from utils import *
 import keras.backend as K
-from keras.engine.topology import Layer
 from keras.callbacks import TensorBoard
 import datetime
 import tensorflow as tf
 import warnings
-
 warnings.filterwarnings('ignore', message='Discrepancy between')
 
 LATENT_DIM = 20
 
 
-class GRI(Layer):
-    def __init__(self, root_node, nodes, edges, **kwargs):
+class BioGAN:
+    def __init__(self, data, latent_dim=LATENT_DIM, max_replay_len=None):
         """
-        :param root_node: node on top of the hierarchy
-        :param nodes: list of nodes (gene symbols) according to which the output nodes will be sorted
-        :param edges: dictionary of edges (keys: *from* node, values: dictionary with key *to* node and
-               value regulation type)
+        Initialize GAN
+        :param data: expression matrix. Shape=(nb_samples, nb_genes)
+        :param latent_dim: number input noise units for the generator
+        :param max_replay_len: size of the replay buffer
         """
-        self._output_dim = len(nodes)
-        self._root_node = root_node
-        self._nodes = nodes
-        self._edges = edges
-        self._r_edges = self._reverse_edges(edges)
-        self._structure = None
-        super(GRI, self).__init__(**kwargs)
-
-    @staticmethod
-    def _reverse_edges(edges):
-        r_edges = {}
-        for tf, tgs in edges.items():
-            for tg, reg_type in tgs.items():
-                if tg in r_edges:
-                    if tf not in r_edges[tg]:
-                        r_edges[tg][tf] = reg_type
-                else:
-                    r_edges[tg] = {tf: reg_type}
-        return r_edges
-
-    def _create_params(self, node, latent_dim, nb_incoming):
-        bias = self.add_weight(name='{}_bias'.format(self._root_node),
-                               shape=(1, 1),
-                               initializer='zeros',
-                               trainable=True)
-        weights = self.add_weight(name='{}_weights'.format(node),
-                                  shape=(latent_dim + nb_incoming, 1),
-                                  initializer='glorot_uniform',
-                                  trainable=True)
-        params = K.concatenate([bias, weights], axis=0)  # Shape=(1 + latent_dim + nb_incoming, 1)
-        return params
-
-    def build(self, input_shape):
-        batch_size, latent_dim = input_shape
-
-        # Structure is a list of tuples with format: (NODE, INCOMING_NODES, WEIGHTS)
-        # The list is sorted so that node i does not depend on node j if j>i
-        w = self._create_params(self._root_node, latent_dim, 0)
-        self._structure = [(self._root_node, [], w)]
-
-        nodes = set(self._nodes) - {self._root_node}
-        remaining = len(nodes)
-        while remaining > 0:
-            for node in nodes:
-                regulated_by = self._r_edges[node].keys()
-                if all([parent not in nodes for parent in regulated_by]):
-                    w = self._create_params(node, latent_dim, len(regulated_by))
-                    t = (node, regulated_by, w)
-                    self._structure.append(t)
-                    nodes = nodes - {node}
-            assert len(nodes) < remaining
-            remaining = len(nodes)
-
-        super(GRI, self).build(input_shape)  # Be sure to call this at the end
-
-    def call(self, z, **kwargs):
-        """
-        :param z: Noise tensor. Shape=(batch_size, LATENT_DIM)
-        :return: synthetic gene expressions tensor
-        """
-        # Dictionary holding the current value of each node in the GRI.
-        # Key: gene symbol. Value: gene Keras tensor
-        units = {}
-
-        # Compute feedforward pass for GRI layer
-        x_bias = K.ones_like(z)  # Shape=(batch_size, latent_dim)
-        x_bias = K.mean(x_bias, axis=-1)[:, None]  # Shape=(batch_size, 1)
-        for t in self._structure:
-            node, incoming, weights = t
-            x_units = [units[in_node] for in_node in incoming]
-            x_in = K.concatenate([x_bias, z] + x_units, axis=-1)  # Shape=(batch_size, 1 + latent_dim + nb_incoming)
-            x_out = K.dot(x_in, weights)  # Shape=(batch_size, 1)
-            units[node] = K.tanh(x_out)
-
-        return K.concatenate([units[node] for node in self._nodes], axis=-1)  # Shape=(batch_size, nb_nodes)
-
-    def compute_output_shape(self, input_shape):
-        return input_shape[0], self._output_dim
-
-
-class BioGAN():
-    def __init__(self, data, root_node, nodes, edges, latent_dim=LATENT_DIM):
         self._latent_dim = latent_dim
         self._data = data
         self._nb_samples, self._nb_genes = data.shape
-        self._root_node = root_node
-        self._nodes = nodes
-        self._edges = edges
-
-        optimizer = Adam(0.0002, 0.5)
-        wass_loss = lambda y_true, y_pred: K.mean(y_true * y_pred)
+        self._max_replay_len = max_replay_len
 
         # Build and compile the discriminator
-        self.discriminator = self.build_discriminator()
+        optimizer = Adam(0.0002, 0.5)
+        self.discriminator = self._build_discriminator()
         self.discriminator.compile(loss='binary_crossentropy',
-                                   optimizer=optimizer,
-                                   metrics=['accuracy'])
-        self._gradients_discr = self.gradients_norm(self.discriminator)
+                                   optimizer=optimizer)
+        self._gradients_discr = self._gradients_norm(self.discriminator)
 
         # Build the generator
-        self.generator = self.build_generator()
-
-        # The generator takes noise as input and generates imgs
+        self.generator = self._build_generator()
         z = Input(shape=(self._latent_dim,))
         gen_out = self.generator(z)
 
-        # For the combined model we will only train the generator
-        self.discriminator.trainable = False
-
-        # The discriminator takes generated images as input and determines validity
-        valid = self.discriminator(gen_out)
-
-        # The combined model  (stacked generator and discriminator)
-        # Trains the generator to fool the discriminator
+        # Build the combined model
         optimizer = Adam(0.0002, 0.5, clipvalue=0.1)
+        self.discriminator.trainable = False
+        valid = self.discriminator(gen_out)
         self.combined = Model(z, valid)
         self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
-        self._gradients_gen = self.gradients_norm(self.combined)
+        self._gradients_gen = self._gradients_norm(self.combined)
 
-    def build_generator(self):
-        # Build generator
+        # Prepare TensorBoard callbacks
+        time_now = str(datetime.datetime.now())
+        log_path = '../logs/discr/{}'.format(time_now)
+        self._callback_discr = TensorBoard(log_path)
+        self._callback_discr.set_model(self.discriminator)
+        log_path = '../logs/gen/{}'.format(time_now)
+        self._callback_gen = TensorBoard(log_path)
+        self._callback_gen.set_model(self.generator)
+
+        # Initialize replay buffer
+        noise = np.random.normal(0, 1, (32, self._latent_dim))
+        x_fake = self.generator.predict(noise)
+        self._replay_buffer = np.array(x_fake)
+
+    def _build_generator(self):
+        """
+        Build the generator
+        """
         noise = Input(shape=(self._latent_dim,))
-        h = Dense(800)(noise)
+        h = Dense(1000)(noise)
         h = LeakyReLU(0.3)(h)
         h = Dropout(0.5)(h)
         h = Dense(self._nb_genes, activation='tanh')(h)
-        # h = GRI(self._root_node, self._nodes, self._edges)(h)
         model = Model(inputs=noise, outputs=h)
         model.summary()
         return model
 
-    def build_discriminator(self):
-        # Build discriminator
+    def _build_discriminator(self):
+        """
+        Build the discriminator
+        """
         expressions_input = Input(shape=(self._nb_genes,))
-        h = Dense(800)(expressions_input)
+        h = Dense(1000)(expressions_input)
         h = LeakyReLU(0.3)(h)
         h = Dropout(0.5)(h)
         h = Dense(1, activation='sigmoid')(h)
@@ -167,17 +88,29 @@ class BioGAN():
         return model
 
     @staticmethod
-    def _write_log(callback, names, logs, batch_nb):
+    def _write_log(callback, names, logs, epoch):
+        """
+        Write log to TensorBoard callback
+        :param callback: TensorBoard callback
+        :param names: list of names for each log. Shape=(nb_logs,)
+        :param logs: list of scalars. Shape=(nb_logs,)
+        :param epoch: epoch number
+        """
         for name, value in zip(names, logs):
             summary = tf.Summary()
             summary_value = summary.value.add()
             summary_value.simple_value = value
             summary_value.tag = name
-            callback.writer.add_summary(summary, batch_nb)
+            callback.writer.add_summary(summary, epoch)
             callback.writer.flush()
 
     @staticmethod
-    def gradients_norm(model):
+    def _gradients_norm(model):
+        """
+        Create Keras function to compute Euclidean norm of gradient vector
+        :param model: Keras model for which the norm will be computed
+        :return: Keras function to compute Euclidean norm of gradient vector
+        """
         grads = K.gradients(model.total_loss, model.trainable_weights)
         summed_squares = tf.stack([K.sum(K.square(g)) for g in grads])
         norm = K.sqrt(K.sum(summed_squares))
@@ -190,27 +123,17 @@ class BioGAN():
 
         return K.function(inputs=input_tensors, outputs=[norm])
 
-    def train(self, epochs, batch_size=32, save_interval=50, max_replay_len=6400):
-        # Prepare TensorBoard callback
-        time_now = str(datetime.datetime.now())
-        log_path = '../logs/discr/{}'.format(time_now)
-        callback_discr = TensorBoard(log_path)
-        callback_discr.set_model(self.discriminator)
-        log_path = '../logs/gen/{}'.format(time_now)
-        callback_gen = TensorBoard(log_path)
-        callback_gen.set_model(self.generator)
-
-        # Initialize replay buffer
-        noise = np.random.normal(0, 1, (batch_size, self._latent_dim))
-        x_fake = self.generator.predict(noise)
-        replay_buffer = np.array(x_fake)
-
-        # Train GAN
+    def train(self, epochs, batch_size=32):
+        """
+        Trains the GAN
+        :param epochs: Number of epochs
+        :param batch_size: Batch size
+        """
         for epoch in range(epochs):
 
-            # ---------------------
+            # ----------------------
             #  Train Discriminator
-            # ---------------------
+            # ----------------------
 
             # Select random samples
             idxs = np.random.randint(0, self._nb_samples, batch_size)
@@ -219,41 +142,46 @@ class BioGAN():
             # Sample noise and generate a batch of new images
             noise = np.random.normal(0, 1, (batch_size, self._latent_dim))
             x_fake = self.generator.predict(noise)
-            mean = 0
-            std = 0.1
-            ln_mean = np.exp(mean + std * std / 2)
-            exp_noise = np.random.lognormal(mean, std, size=x_fake.shape) - ln_mean
-            # x_fake += exp_noise
-
-            # Add data to replay buffer
-            if len(replay_buffer) < max_replay_len:
-                replay_buffer = np.concatenate((replay_buffer, x_fake), axis=0)
-            elif np.random.randint(low=0, high=1) < 0.999 ** epoch:
-                idxs = np.random.choice(len(replay_buffer), batch_size, replace=False)
-                replay_buffer[idxs, :] = x_fake
 
             # Train the discriminator (real classified as ones and generated as zeros)
-            valid = np.random.uniform(low=0.7, high=1, size=(batch_size, 1))
-            fake = np.random.uniform(low=0, high=0.3, size=(batch_size, 1))
+            valid = np.random.uniform(low=0.7, high=1, size=(batch_size, 1))  # Label smoothing
+            fake = np.random.uniform(low=0, high=0.3, size=(batch_size, 1))  # Label smoothing
             d_loss_real = self.discriminator.train_on_batch(x_real, valid)
             d_loss_fake = self.discriminator.train_on_batch(x_fake, fake)
             d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
+            # Add discriminator loss to TensorBoard
+            self._write_log(self._callback_discr, ['train_loss'], [d_loss], epoch)
+
             # Train discr. using replay buffer
-            # idxs = np.random.choice(len(replay_buffer), batch_size)
-            # d_loss_replay = self.discriminator.train_on_batch(replay_buffer[idxs, :], fake)
+            if self._max_replay_len is not None:
+                idxs = np.random.choice(len(self._replay_buffer), batch_size)
+                d_loss_replay = self.discriminator.train_on_batch(self._replay_buffer[idxs, :], fake)
 
-            # ---------------------
+                # Add discriminator loss to TensorBoard
+                self._write_log(self._callback_discr, ['replay_loss'], [d_loss_replay], epoch)
+
+                # Add data to replay buffer
+                if len(self._replay_buffer) < self._max_replay_len:
+                    self._replay_buffer = np.concatenate((self._replay_buffer, x_fake), axis=0)
+                elif np.random.randint(low=0, high=1) < 0.999 ** epoch:
+                    idxs = np.random.choice(len(self._replay_buffer), batch_size, replace=False)
+                    self._replay_buffer[idxs, :] = x_fake
+
+            # ----------------------
             #  Train Generator
-            # ---------------------
+            # ----------------------
 
-            # Train the generator (wants discriminator to mistake images as real)
+            # Train the generator
             valid = np.random.uniform(low=0.7, high=1, size=(batch_size, 1))  # np.ones(shape=(batch_size, 1))
             g_loss = self.combined.train_on_batch(noise, valid)
 
-            # TensorBoard loss
-            self._write_log(callback_discr, ['train_loss'], [d_loss[0]], epoch)
-            self._write_log(callback_gen, ['train_loss'], [g_loss], epoch)
+            # Add generator loss to TensorBoard
+            self._write_log(self._callback_gen, ['train_loss'], [g_loss], epoch)
+
+            # ----------------------
+            #  Report gradient norms
+            # ----------------------
 
             # Gradient norm TensorBoard log
             if epoch % 100 == 0:
@@ -281,24 +209,22 @@ class BioGAN():
                 g_discr = (g_discr_real + g_discr_fake) / 2
 
                 # Add information to TensorBoard log
-                self._write_log(callback_discr, ['gradient_norm'], [g_discr], epoch)
-                self._write_log(callback_gen, ['gradient_norm'], [g_gen], epoch)
+                self._write_log(self._callback_discr, ['gradient_norm'], [g_discr], epoch)
+                self._write_log(self._callback_gen, ['gradient_norm'], [g_gen], epoch)
 
-            # Plot the progress
-            print("%d [D loss: %f] [G loss: %f]" % (epoch, d_loss[0], g_loss))
-
-            # If at save interval => save generated image samples
-            if epoch % save_interval == 0:
-                pass
+            # ----------------------
+            #  Plot the progress
+            # ----------------------
+            print('{} [D loss: {:.4f}] [G loss: {:.4f}]'.format(epoch, d_loss, g_loss))
 
     def generate_batch(self, batch_size=32):
+        """
+        Generate a batch of samples using the generator
+        :param batch_size: Batch size
+        :return: Artificial samples generated by the generator. Shape=(batch_size, nb_genes)
+        """
         noise = np.random.normal(0, 1, (batch_size, self._latent_dim))
         pred = self.generator.predict(noise)
-        mean = 0
-        std = 0.1
-        ln_mean = np.exp(mean + std * std / 2)
-        exp_noise = np.random.lognormal(mean, std, size=pred.shape) - ln_mean
-        # pred += exp_noise
         return pred
 
 
@@ -315,14 +241,6 @@ if __name__ == '__main__':
     expr_train = expr[train_idxs, :]
     expr_test = expr[test_idxs, :]
 
-    # Get regulatory network
-    nodes, edges = reg_network(root_gene=root_gene,
-                               gene_symbols=gene_symbols,
-                               minimum_evidence=minimum_evidence,
-                               max_depth=max_depth,
-                               break_loops=True)
-    root_node = root_gene.lower()
-
     # Clip outliers
     std_clip = 2
     mean = np.mean(expr, axis=0)
@@ -332,16 +250,14 @@ if __name__ == '__main__':
     # Standardize data
     r_min = expr.min(axis=0)
     r_max = expr.max(axis=0)
-    # expr_train = 2*(expr_train - r_min)/(r_max - r_min) - 1
     expr_train = (expr_train - mean) / (std_clip * std)
 
     # Train GAN
-    biogan = BioGAN(expr_train, root_node, gene_symbols, edges)
-    biogan.train(epochs=3000, batch_size=32, save_interval=50)
+    biogan = BioGAN(expr_train, latent_dim=LATENT_DIM)
+    biogan.train(epochs=3000, batch_size=32)
 
     # Generate synthetic data
     s_expr = biogan.generate_batch(expr_train.shape[0])  # expr_test.shape[0]
-    # s_expr = (s_expr + 1)*(r_max - r_min)/2 + r_min
     s_expr = std_clip * s_expr * std + mean
 
     # Save generated data
---
+++ src/layers.py
@@ -0,0 +1,90 @@
+from keras.engine.topology import Layer
+import keras.backend as K
+
+
+class GRI(Layer):
+    def __init__(self, root_node, nodes, edges, **kwargs):
+        """
+        :param root_node: node on top of the hierarchy
+        :param nodes: list of nodes (gene symbols) according to which the output nodes will be sorted
+        :param edges: dictionary of edges (keys: *from* node, values: dictionary with key *to* node and
+               value regulation type)
+        """
+        self._output_dim = len(nodes)
+        self._root_node = root_node
+        self._nodes = nodes
+        self._edges = edges
+        self._r_edges = self._reverse_edges(edges)
+        self._structure = None
+        super(GRI, self).__init__(**kwargs)
+
+    @staticmethod
+    def _reverse_edges(edges):
+        r_edges = {}
+        for tf, tgs in edges.items():
+            for tg, reg_type in tgs.items():
+                if tg in r_edges:
+                    if tf not in r_edges[tg]:
+                        r_edges[tg][tf] = reg_type
+                else:
+                    r_edges[tg] = {tf: reg_type}
+        return r_edges
+
+    def _create_params(self, node, latent_dim, nb_incoming):
+        bias = self.add_weight(name='{}_bias'.format(self._root_node),
+                               shape=(1, 1),
+                               initializer='zeros',
+                               trainable=True)
+        weights = self.add_weight(name='{}_weights'.format(node),
+                                  shape=(latent_dim + nb_incoming, 1),
+                                  initializer='glorot_uniform',
+                                  trainable=True)
+        params = K.concatenate([bias, weights], axis=0)  # Shape=(1 + latent_dim + nb_incoming, 1)
+        return params
+
+    def build(self, input_shape):
+        batch_size, latent_dim = input_shape
+
+        # Structure is a list of tuples with format: (NODE, INCOMING_NODES, WEIGHTS)
+        # The list is sorted so that node i does not depend on node j if j>i
+        w = self._create_params(self._root_node, latent_dim, 0)
+        self._structure = [(self._root_node, [], w)]
+
+        nodes = set(self._nodes) - {self._root_node}
+        remaining = len(nodes)
+        while remaining > 0:
+            for node in nodes:
+                regulated_by = self._r_edges[node].keys()
+                if all([parent not in nodes for parent in regulated_by]):
+                    w = self._create_params(node, latent_dim, len(regulated_by))
+                    t = (node, regulated_by, w)
+                    self._structure.append(t)
+                    nodes = nodes - {node}
+            assert len(nodes) < remaining
+            remaining = len(nodes)
+
+        super(GRI, self).build(input_shape)  # Be sure to call this at the end
+
+    def call(self, z, **kwargs):
+        """
+        :param z: Noise tensor. Shape=(batch_size, LATENT_DIM)
+        :return: synthetic gene expressions tensor
+        """
+        # Dictionary holding the current value of each node in the GRI.
+        # Key: gene symbol. Value: gene Keras tensor
+        units = {}
+
+        # Compute feedforward pass for GRI layer
+        x_bias = K.ones_like(z)  # Shape=(batch_size, latent_dim)
+        x_bias = K.mean(x_bias, axis=-1)[:, None]  # Shape=(batch_size, 1)
+        for t in self._structure:
+            node, incoming, weights = t
+            x_units = [units[in_node] for in_node in incoming]
+            x_in = K.concatenate([x_bias, z] + x_units, axis=-1)  # Shape=(batch_size, 1 + latent_dim + nb_incoming)
+            x_out = K.dot(x_in, weights)  # Shape=(batch_size, 1)
+            units[node] = K.tanh(x_out)
+
+        return K.concatenate([units[node] for node in self._nodes], axis=-1)  # Shape=(batch_size, nb_nodes)
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0], self._output_dim
