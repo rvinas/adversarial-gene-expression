31fdc6b0b2cd9549fb0ee407808b08c21cf13d68
Ramon Viñas Torné <rvinast@gmail.com>
2021-08-19 13:59:52 +0200

    Quick wandb. Checkpoints

--- gtex_tcga_gan.py
+++ gtex_tcga_gan.py
@@ -1,20 +1,24 @@
 from utils import *
 from tf_utils import *
 from collections import Counter
+import pandas as pd
+import numpy as np
+import wandb
 
-
-
-EPOCHS = 1000
 BATCH_SIZE = 32
 LATENT_DIM = 64
-MODELS_DIR = 'checkpoints/models/'
-
+MODELS_DIR = '/local/scratch/rv340/checkpoints/models/'
+CONFIG = {'gpu': 3,
+          'epochs': 2000,
+          'latent_dim': 64,
+          'batch_size': 32,
+          'nb_layers': 2,
+          'hdim': 256,
+          'lr': 5e-4,  #  5e-4,
+          'nb_critic': 5}
 
 
 if __name__ == '__main__':
-    # GPU limit
-    limit_gpu()
-
     # Load dataset
     expr_df, info_df = rnaseqdb_load()
     x = expr_df.values.T
@@ -23,10 +27,9 @@ if __name__ == '__main__':
     tissues = info_df['TISSUE_GTEX'].values
     datasets = info_df['DATASET'].values
 
-    print(Counter(datasets))
-    print(Counter(tissues))
-    print('Nb. genes: ', x.shape[-1])
-    assert False
+    # Log-transform data
+    x = np.log(1 + x)
+    x = np.float32(x)
 
     # Process categorical metadata
     cat_dicts = []
@@ -39,7 +42,6 @@ if __name__ == '__main__':
     datasets = np.vectorize(lambda t: dataset_dict[t])(datasets)
     cat_dicts.append(dataset_dict_inv)
     cat_covs = np.concatenate((tissues[:, None], datasets[:, None]), axis=-1)
-
     cat_covs = np.int32(cat_covs)
     print('Cat covs: ', cat_covs.shape)
 
@@ -48,12 +50,10 @@ if __name__ == '__main__':
     # num_covs = df_metadata.loc[sampl_ids, num_cols].values
     # num_covs = standardize(num_covs)
     # num_covs = np.float32(num_covs)
-    num_covs = np.zeros((x.shape[0], 1), dtype=np.float32)
+    num_covs = np.zeros((x.shape[0], 1), dtype=np.float32)  # TODO: Ignoring for now
     # num_covs = np.zeros_like(cat_covs).astype(np.float32)
-
-    # Log-transform data
-    x = np.log(1 + x)
-    x = np.float32(x)
+    # num_covs = np.copy(x_cond)  # To condition on genes
+    print('Num covs: ', num_covs.shape)
 
     # Train/test split
     np.random.seed(0)
@@ -73,13 +73,25 @@ if __name__ == '__main__':
     x_train = standardize(x_train, mean=x_mean, std=x_std)
     x_test = standardize(x_test, mean=x_mean, std=x_std)
 
+    # Normalise conditioning genes
+    # nc_mean = np.mean(num_covs_train, axis=0)
+    # nc_std = np.std(num_covs_train, axis=0)
+    # num_covs_train = standardize(num_covs_train, mean=nc_mean, std=nc_std)
+    # num_covs_test = standardize(num_covs_test, mean=nc_mean, std=nc_std)
+
+    # GPU limit
+    limit_gpu(CONFIG['gpu'])
+
     # Define model
     vocab_sizes = [len(c) for c in cat_dicts]
     print('Vocab sizes: ', vocab_sizes)
     nb_numeric = num_covs.shape[-1]
     x_dim = x.shape[-1]
-    gen = make_generator(x_dim, vocab_sizes, nb_numeric)
-    disc = make_discriminator(x_dim, vocab_sizes, nb_numeric)
+    gen = make_generator(x_dim, vocab_sizes, nb_numeric,
+                         h_dims=[CONFIG['hdim']] * CONFIG['nb_layers'],
+                         z_dim=CONFIG['latent_dim'])
+    disc = make_discriminator(x_dim, vocab_sizes, nb_numeric,
+                              h_dims=[CONFIG['hdim']] * CONFIG['nb_layers'])
 
     # Evaluation metrics
     def score_fn(x_test, cat_covs_test, num_covs_test):
@@ -101,15 +113,21 @@ if __name__ == '__main__':
 
 
     # Train model
-    gen_opt = tfk.optimizers.RMSprop(5e-4)
-    disc_opt = tfk.optimizers.RMSprop(5e-4)
+    gen_opt = tfk.optimizers.RMSprop(CONFIG['lr'])
+    disc_opt = tfk.optimizers.RMSprop(CONFIG['lr'])
+
+    run = wandb.init(project='adversarial_gene_expr', config=CONFIG)
+    config = wandb.config
+    # wandb.run.name = '{}'.format(wandb.run.name)
+    wandb.run.save()
 
     train(dataset=x_train,
           cat_covs=cat_covs_train,
           num_covs=num_covs_train,
-          z_dim=LATENT_DIM,
-          batch_size=BATCH_SIZE,
-          epochs=EPOCHS,
+          z_dim=CONFIG['latent_dim'],
+          batch_size=CONFIG['batch_size'],
+          epochs=CONFIG['epochs'],
+          nb_critic=CONFIG['nb_critic'],
           gen=gen,
           disc=disc,
           gen_opt=gen_opt,
